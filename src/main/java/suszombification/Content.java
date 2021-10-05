b55d2271cc2adc5291a6e0b999c53f2453f9fbcf
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2021-10-05 19:55:55 +0200

    Added some Candys

--- src/main/java/suszombification/Content.java
+++ src/main/java/suszombification/Content.java
@@ -1,10 +1,12 @@
 package suszombification;
 
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SuspiciousStewItem;
 import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import suszombification.items.CandyItem;
 import suszombification.misc.SuspiciousFoods;
 
 public class Content {
@@ -13,4 +15,11 @@ public class Content {
 
 	//items
 	public static final RegistryObject<Item> SUSPICIOUS_PUMPKIN_PIE = ITEMS.register("suspicious_pumpkin_pie", () -> new SuspiciousStewItem(new Item.Properties().food(SuspiciousFoods.SUSPICIOUS_PUMPKIN_PIE).tab(SuspiciousZombification.suspiciousZombificationGroup)));
+	public static final RegistryObject<Item> CARAMEL_CANDY = ITEMS.register("caramel_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.suspiciousZombificationGroup))); //TODO: assign each candy a unique effect
+	public static final RegistryObject<Item> CHAMOMILE_CANDY = ITEMS.register("chamomile_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.suspiciousZombificationGroup)));
+	public static final RegistryObject<Item> CHOCOLATE_CANDY = ITEMS.register("chocolate_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.suspiciousZombificationGroup)));
+	public static final RegistryObject<Item> CINNAMON_CANDY = ITEMS.register("cinnamon_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.suspiciousZombificationGroup)));
+	public static final RegistryObject<Item> PUMPKIN_CANDY = ITEMS.register("pumpkin_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.suspiciousZombificationGroup)));
+	public static final RegistryObject<Item> VANILLA_CANDY = ITEMS.register("vanilla_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.suspiciousZombificationGroup)));
+	//TODO: MOAR CANDY
 }
---
+++ src/main/java/suszombification/items/CandyItem.java
@@ -0,0 +1,21 @@
+package suszombification.items;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.item.Item;
+
+public class CandyItem extends Item {
+
+	private final MobEffect suspiciousPumpkinPieEffect;
+	private final int effectDuration;
+
+	public CandyItem(MobEffect effect, int effectDuration, Properties properties) {
+		super(properties);
+		this.suspiciousPumpkinPieEffect = effect;
+
+		if (effect.isInstantenous()) {
+			this.effectDuration = effectDuration;
+		} else {
+			this.effectDuration = effectDuration * 20;
+		}
+	}
+}
