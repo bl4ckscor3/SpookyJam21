f69cba289d73c83cd39b3f3ec102d5648aa0d67b
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2021-10-11 20:49:06 +0200

    Added two more candies and mapped effects

--- src/main/java/suszombification/SZItems.java
+++ src/main/java/suszombification/SZItems.java
@@ -16,13 +16,16 @@ public class SZItems {
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SuspiciousZombification.MODID);
 
 	public static final RegistryObject<Item> SUSPICIOUS_PUMPKIN_PIE = ITEMS.register("suspicious_pumpkin_pie", () -> new SuspiciousPumpkinPieItem(new Item.Properties().food(new FoodProperties.Builder().nutrition(8).saturationMod(0.3F).alwaysEat().build()).tab(SuspiciousZombification.TAB)));
-	public static final RegistryObject<Item> CARAMEL_CANDY = ITEMS.register("caramel_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.TAB))); //TODO: assign each candy a unique effect
-	public static final RegistryObject<Item> CHAMOMILE_CANDY = ITEMS.register("chamomile_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.TAB)));
-	public static final RegistryObject<Item> CHOCOLATE_CANDY = ITEMS.register("chocolate_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.TAB)));
-	public static final RegistryObject<Item> CINNAMON_CANDY = ITEMS.register("cinnamon_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.TAB)));
-	public static final RegistryObject<Item> PUMPKIN_CANDY = ITEMS.register("pumpkin_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.TAB)));
-	public static final RegistryObject<Item> VANILLA_CANDY = ITEMS.register("vanilla_candy", () -> new CandyItem(MobEffects.GLOWING, 5, new Item.Properties().tab(SuspiciousZombification.TAB)));
-	//TODO: MOAR CANDY
+	public static final RegistryObject<Item> CARAMEL_CANDY = ITEMS.register("caramel_candy", () -> new CandyItem(MobEffects.SLOW_FALLING, 20, new Item.Properties().tab(SuspiciousZombification.TAB)));
+	public static final RegistryObject<Item> CHAMOMILE_CANDY = ITEMS.register("chamomile_candy", () -> new CandyItem(MobEffects.WATER_BREATHING, 20, new Item.Properties().tab(SuspiciousZombification.TAB)));
+	public static final RegistryObject<Item> CHOCOLATE_CANDY = ITEMS.register("chocolate_candy", () -> new CandyItem(MobEffects.DIG_SLOWDOWN, 20, new Item.Properties().tab(SuspiciousZombification.TAB)));
+	public static final RegistryObject<Item> CINNAMON_CANDY = ITEMS.register("cinnamon_candy", () -> new CandyItem(MobEffects.GLOWING, 20, new Item.Properties().tab(SuspiciousZombification.TAB)));
+	public static final RegistryObject<Item> HONEY_CANDY = ITEMS.register("honey_candy", () -> new CandyItem(MobEffects.DAMAGE_BOOST, 20, new Item.Properties().tab(SuspiciousZombification.TAB)));
+	public static final RegistryObject<Item> PINEAPPLE_CANDY = ITEMS.register("pineapple_candy", () -> new CandyItem(MobEffects.LEVITATION, 20, new Item.Properties().tab(SuspiciousZombification.TAB)));
+	public static final RegistryObject<Item> PUMPKIN_CANDY = ITEMS.register("pumpkin_candy", () -> new CandyItem(MobEffects.INVISIBILITY, 20, new Item.Properties().tab(SuspiciousZombification.TAB)));
+	public static final RegistryObject<Item> VANILLA_CANDY = ITEMS.register("vanilla_candy", () -> new CandyItem(MobEffects.DIG_SPEED, 20, new Item.Properties().tab(SuspiciousZombification.TAB)));
+	//TODO: maybe more candy flavours?
+	//TODO: short monolougle-like texts in chat when eating a SPP? Like "The taste of this pie makes you wanna go mine diamonds" for Vanilla-Candy-SSP or "Urgh, this pie probably wasn't meant to be eaten by a human" for a mob-drop SPP
 	public static final RegistryObject<Item> ROTTEN_MILK_BUCKET = ITEMS.register("rotten_milk_bucket", () -> new RottenMilkBucketItem(new Item.Properties().stacksTo(1).tab(SuspiciousZombification.TAB)));
 	public static final RegistryObject<Item> ROTTEN_EGG = ITEMS.register("rotten_egg", () -> new RottenEggItem(new Item.Properties().stacksTo(16).tab(SuspiciousZombification.TAB)));
 	public static final RegistryObject<Item> ZOMBIFIED_CHICKEN_SPAWN_EGG = ITEMS.register("zombified_chicken_spawn_egg", () -> new ForgeSpawnEggItem(SZEntityTypes.ZOMBIFIED_CHICKEN, 0xA1A1A1, 0x799C65, new Item.Properties().tab(SuspiciousZombification.TAB)));
