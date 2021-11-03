84ff8b478e1b027435d93bf3060127360d37a2b8
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-11-03 11:31:34 +0100

    add more restrictions to zombie's curse
    don't affect named animals
    don't affect tamed animals
    don't affect animals spawned from a bucket

--- src/main/java/suszombification/effect/ZombiesCurseEffect.java
+++ src/main/java/suszombification/effect/ZombiesCurseEffect.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.TamableAnimal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.Bucketable;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import suszombification.SZEffects;
@@ -14,7 +16,14 @@ public class ZombiesCurseEffect extends VicinityAffectingEffect {
 	public ZombiesCurseEffect(MobEffectCategory category, int color) {
 		super(category, color,
 				amplifier -> 15,
-				e -> !e.getType().is(SZTags.EntityTypes.AFFECTED_BY_ZOMBIES_GRACE) && !e.hasEffect(SZEffects.DECOMPOSING.get()) && (e instanceof Player || e instanceof Animal),
+				e -> { return
+						!e.getType().is(SZTags.EntityTypes.AFFECTED_BY_ZOMBIES_GRACE)
+						&& !e.hasEffect(SZEffects.DECOMPOSING.get())
+						&& (e instanceof Player || (e instanceof Animal a
+								&& !a.hasCustomName() //don't affect animals with name tags
+								&& (!(a instanceof TamableAnimal ta) || !ta.isTame()) //don't affect tamed animals
+								&& (!(a instanceof Bucketable b) || !b.fromBucket()))); //don't affect animals that were spawned from a bucket
+				},
 				() -> new MobEffectInstance(SZEffects.DECOMPOSING.get(), 300));
 	}
 
