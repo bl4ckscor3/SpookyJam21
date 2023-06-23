24e6d3254ec2bcd3bff5e2cef3a17b8c73273ce0
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2023-06-23 16:29:39 +0200

    Ran runData, removed todo comment

--- src/main/java/suszombification/glm/CatMorningGiftModifier.java
+++ src/main/java/suszombification/glm/CatMorningGiftModifier.java
@@ -23,7 +23,6 @@ public class CatMorningGiftModifier extends LootModifier {
 
 	@Override
 	protected ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-		//TODO: test if this is correct
 		return context.getLevel().getServer().getLootData().getLootTable(SZLoot.ZOMBIFIED_CAT_MORNING_GIFT).getRandomItems(context);
 	}
 
