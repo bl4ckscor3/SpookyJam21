2bb2c78302ed5421f8c4cf34112c2e10e5c5965e
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-03-24 11:08:46 +0100

    make use of the vanilla zombies entity type tag
    closes #2

--- src/main/java/suszombification/datagen/EntityTypeTagGenerator.java
+++ src/main/java/suszombification/datagen/EntityTypeTagGenerator.java
@@ -6,7 +6,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.EntityTypeTagsProvider;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.tags.EntityTypeTags;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import suszombification.SZTags;
 import suszombification.SuspiciousZombification;
@@ -20,14 +20,8 @@ public class EntityTypeTagGenerator extends EntityTypeTagsProvider {
 	@Override
 	protected void addTags(HolderLookup.Provider provider) {
 		//@formatter:off
-		tag(SZTags.EntityTypes.AFFECTED_BY_ZOMBIES_GRACE).add(
-				EntityType.DROWNED,
-				EntityType.HUSK,
-				EntityType.ZOGLIN,
-				EntityType.ZOMBIE,
-				EntityType.ZOMBIE_HORSE,
-				EntityType.ZOMBIE_VILLAGER,
-				EntityType.ZOMBIFIED_PIGLIN,
+		tag(SZTags.EntityTypes.AFFECTED_BY_ZOMBIES_GRACE).addTag(EntityTypeTags.ZOMBIES);
+		tag(EntityTypeTags.ZOMBIES).add(
 				SZEntityTypes.ZOMBIFIED_CAT.get(),
 				SZEntityTypes.ZOMBIFIED_CHICKEN.get(),
 				SZEntityTypes.ZOMBIFIED_COW.get(),
