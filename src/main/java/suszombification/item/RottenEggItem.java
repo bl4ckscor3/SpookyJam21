d5c1b82c1f6ed512ec805a0339731e9799091012
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-05-08 15:07:02 +0200

    fix game not starting

--- src/main/java/suszombification/item/RottenEggItem.java
+++ src/main/java/suszombification/item/RottenEggItem.java
@@ -1,18 +1,22 @@
 package suszombification.item;
 
+import net.minecraft.core.Direction;
+import net.minecraft.core.Position;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ProjectileItem;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
 import suszombification.entity.ThrownRottenEgg;
 
-public class RottenEggItem extends Item {
+public class RottenEggItem extends Item implements ProjectileItem {
 	public RottenEggItem(Properties properties) {
 		super(properties);
 
@@ -39,5 +43,13 @@ public class RottenEggItem extends Item {
 			stack.shrink(1);
 
 		return InteractionResultHolder.sidedSuccess(stack, level.isClientSide());
+	}
+
+	@Override
+	public Projectile asProjectile(Level level, Position pos, ItemStack stack, Direction direction) {
+		ThrownRottenEgg egg = new ThrownRottenEgg(level, pos.x(), pos.y(), pos.z());
+
+		egg.setItem(stack);
+		return egg;
 	}
 }
